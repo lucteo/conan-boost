17a1c121497b07fd5443e180f88cb0fba66b97d7
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2016-09-25 00:07:16 +0300

    Changed compilation to work with gcc 6.2
    Don’t define _GLIBCXX_USE_CXX11_ABI=0

--- conanfile.py
+++ conanfile.py
@@ -101,18 +101,22 @@ class BoostConan(ConanFile):
         
         # LIBCXX DEFINITION FOR BOOST B2
         try:
-            if str(self.settings.compiler.libcxx) == "libstdc++":
-                flags.append("define=_GLIBCXX_USE_CXX11_ABI=0")
-            elif str(self.settings.compiler.libcxx) == "libstdc++11":
-                flags.append("define=_GLIBCXX_USE_CXX11_ABI=1")
+            if "gcc" in str(self.settings.compiler):
+                if str(self.settings.compiler.version) == "6.2":
+                    cxx_flags.append("-std=c++14")
+                else:
+                    cxx_flags.append("-std=c++11")
             if "clang" in str(self.settings.compiler):
+                if str(self.settings.compiler.version) == "8.0":
+                    cxx_flags.append("-std=c++14")
+                else:
+                    cxx_flags.append("-std=c++11")
+
                 if str(self.settings.compiler.libcxx) == "libc++":
                     cxx_flags.append("-stdlib=libc++")
-                    cxx_flags.append("-std=c++11")
                     flags.append('linkflags="-stdlib=libc++"')
                 else:
                     cxx_flags.append("-stdlib=libstdc++")
-                    cxx_flags.append("-std=c++11")
         except:
             pass
       
